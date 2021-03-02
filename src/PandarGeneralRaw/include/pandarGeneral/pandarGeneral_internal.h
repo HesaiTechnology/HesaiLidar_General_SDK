a3e42fe87333bac9027063d8ed9474df28ece8b1
张煜 <zhangyu@hesaitech.com>
2021-03-02 19:29:56 +0800

    delete repetitive code

--- src/PandarGeneralRaw/include/pandarGeneral/pandarGeneral_internal.h
+++ src/PandarGeneralRaw/include/pandarGeneral/pandarGeneral_internal.h
@@ -293,14 +293,7 @@ typedef struct{
 typedef struct {
     unsigned int id; //!< Detection object identification id
     uint64_t timestamp; //!< The timestamp of the end of object detection
-    float yaw[4]; // Quadruple o#define XT_COORDINATE_CORRECTION_H (31.5 / 1000 / 1000)
-#define XT_COORDINATE_CORRECTION_B (13.0 / 1000 / 1000)
-#define QT_COORDINATE_CORRECTION_ODOG (29.8 / 1000 / 1000)
-#define QT_COORDINATE_CORRECTION_ODOT (7.2 / 1000 / 1000)
-#define QT_COORDINATE_CORRECTION_F (29.5 / 1000 / 1000)
-#define QT_COORDINATE_CORRECTION_I0 (0.6 / 1000 / 1000)
-#define QT_COORDINATE_CORRECTION_S0 (0.17 / 1000 / 1000)
-#define QT_COORDINATE_CORRECTION_D0 (20)f object pose
+    float yaw[4]; // Quadruple of object pose
     float rect_x; //!< Object detection frame size xyz(m)
     float rect_y; //!< Object detection frame size xyz(m)
     float rect_z; //!< Object detection frame size xyz(m)
